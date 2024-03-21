54a968f73912c9ec5cd010c123553605aefd32f8
Y0U5S3F <akim25486@gmail.com>
2024-03-21 11:53:53 +0100

    fixed node modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { ApiService } from './api.service'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [
-    provideClientHydration(),
-    ApiService
+    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }import { HttpClientModule } from '@angular/common/http';
-
+export class AppModule { }
