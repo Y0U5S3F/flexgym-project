92bcea654766d4ca8b69b4c335313c8dea25b2ee
Y0U5S3F <akim25486@gmail.com>
2024-03-21 12:51:42 +0100

    components

---
+++ angular/src/app/components/choose/choose.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose',
+  templateUrl: './choose.component.html',
+  styleUrl: './choose.component.css'
+})
+export class ChooseComponent {
+
+}
---
+++ angular/src/app/components/join/join.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join',
+  templateUrl: './join.component.html',
+  styleUrl: './join.component.css'
+})
+export class JoinComponent {
+
+}
