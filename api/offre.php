599437e4759ab6a8cf7ff55dcd9dc73792dfeea9
Youssef Saidani <akim25486@gmail.com>
2024-03-22 12:58:44 +0100

    offrePHP

---
+++ api/offre.php
@@ -0,0 +1,47 @@
+<?php
+
+$var = $_SERVER['REQUEST_METHOD'];
+require 'connect.php';
+require 'charger/chargerOffre.php';
+require 'supprimer/supprimerOffre.php';
+require 'ajouter/ajouterOffre.php';
+require 'modifier/modifierOffre.php';
+header("Content-type:application/json");
+
+switch ($var) {
+    case 'GET':
+        if(isset($_GET["id"]) && ($_GET['id'] != null)) {
+            $id = $_GET["id"];
+            chargerClient($id);
+        } else {
+            chargerTousClient();
+        }
+        break;
+    case 'DELETE':
+        if(isset($_GET["id"]) && ($_GET["id"] != null)) {
+            $id = $_GET["id"];
+            supprimerClient($id);
+        }
+        break;
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'), true);
+        if ($data) {
+            ajouterClient($data);
+        } else {
+            http_response_code(400);
+            echo json_encode(array("Error: " => "Invalid JSON data"));
+        }
+        break;
+    case 'PUT':
+        $data = json_decode(file_get_contents('php://input'), true);
+        if ($data && isset($_GET["id"]) && ($_GET["id"] != null)) {
+            $id = $_GET["id"];
+            modifierClient($id, $data);
+        } else {
+            http_response_code(400);
+            echo json_encode(array("Error: " => "Invalid JSON data or missing ID"));
+        }
+        break;
+
+}
+
